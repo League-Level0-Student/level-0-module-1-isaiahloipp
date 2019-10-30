d8626c749db898ab00b1f0a48014e41f6c27da19
i-lab user <i-labuser@SDPL88536-iMacProLO.local>
2019-10-29 17:52:21 -0700

    Remarkable

---
+++ src/_02_unbirthday/UNbirthday.java
@@ -0,0 +1,36 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class UNbirthday {
+public static void main(String[] args) {
+	String input = JOptionPane.showInputDialog(null,"when is your birthday");
+	if(input.equals("10/29")) {
+		JOptionPane.showMessageDialog(null,"happy birthday");
+	}else {JOptionPane.showMessageDialog(null,"happy UNbirthday");
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
