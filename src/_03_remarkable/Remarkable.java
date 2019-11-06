f4013428c4b2583e4e8ead6fe049eb133edc1ac3
i-lab user <i-labuser@SDPL88535-iMacProLO.local>
2019-11-05 17:56:19 -0800

    Done with remarkable

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,11 +8,53 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String Alex="Your Smart";
+String Nicole="you have an academic attitude";	
+String Rickey="Your nice";	
+String Adora="Your caring";
+String Rodrigo="Your awsome";	
+String Michiyo="Your brilliant";	
+String Matthew="Your goal oriented";
+String Sebastian="Your Excellent";	
+String names=JOptionPane.showInputDialog(null,"what Is Your name");
+	
+	
+	
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+if(names.equals("Alex")) {JOptionPane.showMessageDialog(null,Alex);
+}	
+else if(names.equals("Nicole")) {JOptionPane.showMessageDialog(null,Nicole);
+}	
+else if(names.equals("Rickey")) {JOptionPane.showMessageDialog(null,Rickey);
+}	
+else if(names.equals("Adora")) {JOptionPane.showMessageDialog(null,Adora);
+}		
+else if(names.equals("Rodrigo")) {JOptionPane.showMessageDialog(null,Rodrigo);
+}		
+else if(names.equals("Michiyo")) {JOptionPane.showMessageDialog(null,Michiyo);
+}	
+else if(names.equals("Matthew")) {JOptionPane.showMessageDialog(null,Matthew);
+}	
+else if(names.equals("Sebastian")) {JOptionPane.showMessageDialog(null,Sebastian);
+}	
+else {
+	JOptionPane.showMessageDialog(null,"I don't know you I Guess you can still be awsome");
+}
+	
+	
+	
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
 
